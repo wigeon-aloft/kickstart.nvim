3720108d68011873fd3ac3bc6dcc6689b7c788e0
wigeon-aloft <willem@thevanhoofs.com>
2024-07-30 00:15:23 +0100

    Updated which-key keymaps to follow new spec

--- init.lua
+++ init.lua
@@ -246,20 +246,28 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>o'] = { name = 'Edit[o]r', _ = 'which_key_ignore' },
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      local wk = require 'which-key'
+      wk.add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>d_', hidden = true },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>d_', hidden = true },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>d_', hidden = true },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>d_', hidden = true },
+        { '<leader>h', group = 'Git' },
+        { '<leader>d_', hidden = true },
       }
+
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      wk.add {
+        { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+      }
     end,
   },
 

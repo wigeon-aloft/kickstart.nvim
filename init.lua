0bed4202e9375573fa825619563b75be8f5ea422
Wigeon <willem@thevanhoofs.com>
2024-08-12 16:56:42 +0100

    Updated neovide refresh rate settings to 165Hz

--- init.lua
+++ init.lua
@@ -913,6 +913,6 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0.2
   vim.g.neovide_cursor_animation_length = 0.1
   vim.g.neovide_cursor_trail_size = 0.5
-  vim.g.neovide_refresh_rate = 90
+  vim.g.neovide_refresh_rate = 165
   vim.g.neovide_cursor_smooth_blink = true
 end

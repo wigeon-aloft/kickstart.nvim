7254b3a6f687283f9519f7bf15d1e563c4e98646
Wigeon <willem@thevanhoofs.com>
2024-09-23 18:11:15 +0100

    Segmented plugin and neovide configurations into separate files

---
+++ lua/neovide.lua
@@ -0,0 +1,6 @@
+vim.g.neovide_scale_factor = 0.8
+vim.g.neovide_scroll_animation_length = 0.2
+vim.g.neovide_cursor_animation_length = 0.1
+vim.g.neovide_cursor_trail_size = 0.5
+vim.g.neovide_refresh_rate = 165
+vim.g.neovide_cursor_smooth_blink = true

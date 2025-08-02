bd659e11986e29ae54209c48a9428c6c6578cdae
wigeon-aloft <willem@thevanhoofs.com>
2025-08-02 16:43:46 +0100

    created custom themes directory

--- lua/custom/plugins/gruvbox.lua
+++
@@ -1,8 +0,0 @@
--- gruvbox colorscheme
-return {
-  'morhetz/gruvbox',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    vim.cmd.colorscheme 'gruvbox'
-  end,
-}
---
+++ lua/custom/themes/catppuccin.lua
@@ -0,0 +1,17 @@
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		flavour = "mocha",
+		background = {
+			light = "latte",
+			dark = "mocha",
+		},
+		auto_integrations = true,
+		term_colors = false,
+		color_overrides = {
+			mocha = {},
+		},
+	},
+}

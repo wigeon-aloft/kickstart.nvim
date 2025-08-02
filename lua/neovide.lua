97d20dbd2f0c006cedb40a69100de91bcf1945d5
wigeon-aloft <willem@thevanhoofs.com>
2025-08-02 16:41:50 +0100

    updated lua scale factor

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,4 +1,4 @@
-vim.g.neovide_scale_factor = 0.9
+vim.g.neovide_scale_factor = 0.8
 vim.g.neovide_scroll_animation_length = 0.2
 vim.g.neovide_cursor_animation_length = 0.1
 vim.g.neovide_cursor_trail_size = 0.5
